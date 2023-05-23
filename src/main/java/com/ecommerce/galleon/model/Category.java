55f18f1f85f63afadd7a8ce42321f20159e5c342
RADMAN_HAYATI <radman.hayatii@gmail.com>
2023-05-23 10:51:05 +0430

    Category model defined

--- src/main/java/com/ecommerce/galleon/model/Category.java
+++ src/main/java/com/ecommerce/galleon/model/Category.java
@@ -1,4 +1,54 @@
 package com.ecommerce.galleon.model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private @NotBlank String categoryName;
+
+    private @NotBlank String description;
+
+    @Column(name = "image_url")
+    private @NotBlank String imageUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
