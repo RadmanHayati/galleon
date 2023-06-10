e4ba1bf1d13b7d4a45215b6b2ccfea9461475409
RADMAN_HAYATI <radman.hayatii@gmail.com>
2023-06-10 05:58:28 +0330

    Product added.

---
+++ src/main/java/com/ecommerce/galleon/controller/ProductController.java
@@ -0,0 +1,54 @@
+package com.ecommerce.galleon.controller;
+
+import com.ecommerce.galleon.common.ApiResponse;
+import com.ecommerce.galleon.dto.ProductDto;
+import com.ecommerce.galleon.model.Category;
+import com.ecommerce.galleon.repository.CategoryRepository;
+import com.ecommerce.galleon.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    CategoryRepository categoryRepo;
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> createProduct(@RequestBody ProductDto productDto) {
+        Optional<Category> optionalCategory = categoryRepo.findById(productDto.getCategoryId());
+        if (!optionalCategory.isPresent()) {
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exists"), HttpStatus.BAD_REQUEST);
+        }
+        productService.createProduct(productDto, optionalCategory.get());
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "product has been added"), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        List<ProductDto> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    // create an api to edit the product
+
+
+    @PostMapping("/update/{productId}")
+    public ResponseEntity<ApiResponse> updateProduct(@PathVariable("productId") Integer productId, @RequestBody ProductDto productDto) throws Exception {
+        Optional<Category> optionalCategory = categoryRepo.findById(productDto.getCategoryId());
+        if (!optionalCategory.isPresent()) {
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exists"), HttpStatus.BAD_REQUEST);
+        }
+        productService.updateProduct(productDto, productId);
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "product has been updated"), HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/ecommerce/galleon/model/Product.java
@@ -0,0 +1,69 @@
+package com.ecommerce.galleon.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+
+    private @NotNull String name;
+    private @NotNull String imageURL;
+    private @NotNull double price;
+    private @NotNull String description;
+
+
+    // Many to one relationship
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    Category category;
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
