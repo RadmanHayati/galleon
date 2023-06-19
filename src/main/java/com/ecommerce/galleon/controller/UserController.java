b4072aed52ee26cf1eac4aff6f336ed03f15a5fb
RADMAN_HAYATI <radman.hayatii@gmail.com>
2023-06-20 00:21:33 +0330

    Auth started.

---
+++ src/main/java/com/ecommerce/galleon/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.ecommerce.galleon.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("user")
+@RestController
+public class UserController {
+/*
+
+    @Autowired
+    UserService userService;
+
+    // two apis
+
+    // signup
+
+    @PostMapping("/signup")
+    public ResponseDto signup(@RequestBody SignupDto signupDto) {
+        return userService.signUp(signupDto);
+    }
+
+
+    // signin
+
+    @PostMapping("/signin")
+    public SignInReponseDto signIn(@RequestBody SignInDto signInDto) {
+        return userService.signIn(signInDto);
+    }
+*/
+
+
+}
